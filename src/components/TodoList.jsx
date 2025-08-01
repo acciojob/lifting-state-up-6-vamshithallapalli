af61bc11c6dff4a60ecdee9391ed898ae84bbaf7
vamshithallapalli <thallapallivamshi50@gmail.com>
2025-08-01 15:40:44 +0530

    rectified

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const TodoList = ({todos, handleComplete}) => {
-
+const TodoList = ({ todos, handleComplete }) => {
   return (
-    <ul style={{ listStyleType: "none", padding: 0 }}>
-      {todos.map((todo) => (
-        <li key={todo.id} style={{ marginBottom: "10px" }}>
-          <span
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none",
-              marginRight: "10px",
-            }}
-          >
-            {todo.text}
-          </span>
-          {!todo.completed && (
-            <button onClick={() => handleComplete(todo.id)}>Complete</button>
-          )}
-        </li>
-      ))}
-    </ul>
-  )
-}
+    <>
+      <h2>Child Component</h2>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {todos.map((todo) => (
+          <li key={todo.id} style={{ marginBottom: "10px" }}>
+            <span
+              style={{
+                marginRight: "10px",
+              }}
+            >
+              {todo.text}
+            </span>
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default TodoList
+export default TodoList;
